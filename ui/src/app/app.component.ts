c8bb1ff0ad314dc23cea5e3896dae5d4ad36e49b
kode-emperor <kode.emperor@gmail.com>
2024-09-06 07:00:36 +0100

    added routing [login,chat]

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatComponent],
+  imports: [RouterOutlet, ChatComponent, LoginComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'srv';
+
+  constructor(private router: Router) {
+
+  }
+  
 }
--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'login'
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
